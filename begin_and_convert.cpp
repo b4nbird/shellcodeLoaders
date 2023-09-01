a8ca3277468518dc9900b02f9ac3248170cd46f5
b4nbird <63333153+b4nbird@users.noreply.github.com>
2023-09-01 11:43:38 +0800

    Update begin_and_convert.cpp

--- begin_and_convert.cpp
+++ begin_and_convert.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <Windows.h>
 
+//原始shellcode过滤成字符串
+/*
+"\xac\xv2\x3f"
+"\xac\xv2\x3f"
+"\xac\xv2\x3f"
+"\xac\xv2\x3f"
+"\xac\xv2\x3f"
+*/
+void Compressed(const char* FileName)
+{
+    FILE* fp_read;
+    char write_ch;
+    if ((fp_read = fopen(FileName, "r")) != NULL)
+    {
+        while ((write_ch = fgetc(fp_read)) != EOF)
+        {
+            if (write_ch != L'\n' && write_ch != L'\"' && write_ch != L'\\' && write_ch != L'x' && write_ch != L';')
+            {
+                printf("%c", write_ch);
+            }
+        }
+    }
+    _fcloseall();
+}
+
 int main(int argc, char* argv[])
 {
     unsigned int char_in_hex;
